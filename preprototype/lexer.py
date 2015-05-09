44d68e86adaaac3ddd1930bd0d08f76d3a43102a
Zack M. Davis <code@zackmdavis.net>
2015-05-09 15:32:18 -0700

    allow recognition of string literals
    It turns out that we need to be able to recognize not just if a
    substring of the source is a valid token, but whether it could be the
    beginning of a valid token.

--- preprototype/lexer.py
+++ preprototype/lexer.py
@@ -17,6 +17,9 @@ class Token:
     def match(cls, source_fragment):
         if cls.recognizer.match(source_fragment):
             return cls(source_fragment)
+        elif getattr(cls, 'prefix_recognizer',
+                     re.compile('$^')).match(source_fragment):
+            return True  # not a valid match, but the prefix of one
         else:
             return None
 
@@ -42,9 +45,11 @@ class CloseParenthesis(CloseDelimiter):
     recognizer = re.compile(r"\)$")
 
 class StringLiteral(Token):
+    prefix_recognizer = re.compile(r'"[^"]*$')
     recognizer = re.compile(r'".*"$')
 
 class InternLiteral(Token):
+    prefix_recognizer = re.compile(r"'[^']*$")
     recognizer = re.compile(r"'.*'$")
 
 class IntegerLiteral(Token):
@@ -94,20 +99,19 @@ class BaseTokenizer:
                 sight = premonition
                 candidate_end += 1
             else:
-                if len(sight) == 1:
+                if len(sight) == 1 and isinstance(sight[0], Token):
                     self.tokens.append(sight[0])
                     candidate_start = candidate_end - 1
-                    while source[candidate_start] == ' ':
+                    while source[candidate_start] in ' \n\t':
                         candidate_start += 1  # skip whitespace
                     candidate_end = candidate_start + 1
                 else:
                     self._handle_tokenizing_error(sight, candidate)
         return self.tokens
 
+TOKENCLASSES = [Identifier, OpenParenthesis, CloseParenthesis,
+                StringLiteral, InternLiteral, IntegerLiteral, EndOfFile]
 
 class Tokenizer(BaseTokenizer):
     def __init__(self):
-        super().__init__(
-            [Identifier, OpenParenthesis, CloseParenthesis,
-             StringLiteral, InternLiteral, IntegerLiteral, EndOfFile]
-        )
+        super().__init__(TOKENCLASSES)
--- preprototype/tests/test_lexer.py
+++ preprototype/tests/test_lexer.py
@@ -24,12 +24,12 @@ class TokenClassMatchingTest(unittest.TestCase):
 
 class TokenizerTest(unittest.TestCase):
 
-    def test_tokenize_codeform_of_identifiers_and_integer(self):
+    def test_tokenize_codeform(self):
         self.assertEqual(
-            Tokenizer().tokenize("(foo bar 3)"),
+            Tokenizer().tokenize("(foo 'bar' \"quux\" 3)"),
             [OpenParenthesis("("), Identifier("foo"),
-             Identifier("bar"), IntegerLiteral("3"),
-             CloseParenthesis(")")]
+             InternLiteral("'bar'"), StringLiteral('"quux"'),
+             IntegerLiteral("3"), CloseParenthesis(")")]
         )
 
 if __name__ == "__main__":
