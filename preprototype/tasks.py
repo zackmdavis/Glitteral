0b6990de4daf4044de5ecd2f92003f1adc96222c
Zack M. Davis <code@zackmdavis.net>
2015-06-06 20:41:04 -0700

    more Invoke helpers

--- preprototype/tasks.py
+++ preprototype/tasks.py
@@ -3,3 +3,15 @@ from invoke import task, run
 @task
 def clean():
     run("rm __glitteral_compiled.rs demo")
+    print("clean!")
+
+def compile(sourcefile, ir=False):
+    run("./glitteralc {} {}".format(sourcefile, "--just-rust" if ir else ''))
+
+@task
+def demo(ir=False):
+    compile("demo.gltrl", ir=ir)
+
+@task
+def eg(example, ir=False):
+    compile("../eg/{}.gltrl".format(example), ir=ir)
