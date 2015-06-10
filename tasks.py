3a309a5c526ea7cb5f85853773a989d5afd80b51
Zack M. Davis <code@zackmdavis.net>
2015-06-10 09:57:59 -0700

    test and harness for expected output of examples

--- tasks.py
+++ tasks.py
@@ -1,6 +1,7 @@
 import os
 from invoke import task, run
 
+# TODO: robustness to other current working directories
 
 def compile(sourcefile, ir=False):
     run("bin/glitteralc {} {}".format(sourcefile, "--just-rust" if ir else ''),
@@ -8,21 +9,30 @@ def compile(sourcefile, ir=False):
         # connected to an actual terminal
         pty=True)
 
+def eg_executable_paths():
+    for filename in os.listdir("eg"):
+        filepath = os.path.join("eg", filename)
+        if os.access(filepath, os.X_OK) and not os.path.isdir(filepath):
+            yield filepath
+
 @task
 def clean():
     # clean generated Rust
+    print("cleaning generated Rust ...")
     run("rm -f preprototype/__*_compiled.rs eg/__*_compiled.rs")
     # clean compiled executables
+    print("cleaning executables ...")
     run("rm -f preprototype/demo")
-    for filename in os.listdir("eg"):
-        filepath = os.path.join("eg", filename)
-        if os.access(filepath, os.X_OK):
-            os.unlink(filepath)
+    for executable_path in eg_executable_paths():
+        os.unlink(executable_path)
     print("clean!")
 
 @task
 def test():
+    print("running internal tests ..")
     run("cd preprototype/tests; python3 -m unittest")
+    print("running output tests ...")
+    run("cd eg/tests; python3 -m unittest")
 
 @task
 def demo(ir=False):
@@ -30,4 +40,5 @@ def demo(ir=False):
 
 @task
 def eg(example, ir=False):
+    print("compiling {} ...".format(example))
     compile("eg/{}.gltrl".format(example), ir=ir)
