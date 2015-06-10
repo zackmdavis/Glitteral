cfe2b5ea08be49f0bb0504f3d99d4fa22f768371
Zack M. Davis <code@zackmdavis.net>
2015-06-09 18:07:38 -0700

    yank prewritten Rust builtins from backend.py to separate file

--- preprototype/backend.py
+++ preprototype/backend.py
@@ -219,72 +219,15 @@ def generate_expression(expression):
 
 def generate_code(expressions):
     logger.debug("expressions for which to generate code: %s", expressions)
-    return """
-use std::fmt::Display;
-use std::collections::HashMap;
-
-// Glitteral standard library arithmetic
-fn integers_equal(a: isize, b: isize) -> bool { a == b }
-fn integers_not_equal(a: isize, b: isize) -> bool { a != b }
-fn add_integers(a: isize, b: isize) -> isize { a + b }
-fn subtract_integers(a: isize, b: isize) -> isize { a - b }
-fn multiply_integers(a: isize, b: isize) -> isize { a * b }
-fn divide_integers(a: isize, b: isize) -> isize { a / b }
-fn greater(a: isize, b: isize) -> bool { a > b }
-fn less(a: isize, b: isize) -> bool { a < b }
-fn not_less(a: isize, b: isize) -> bool { a >= b }
-fn not_greater(a: isize, b: isize) -> bool { a <= b }
-
-// more builtins
-fn append(list: &mut Vec<isize>, item: isize) -> &mut Vec<isize> {
-    list.push(item);
-    list
-}
-fn length<T>(container: &Vec<T>) -> isize {
-    container.len() as isize
-}
-fn range(start: isize, end: isize) -> Vec<isize> {
-    let mut items = vec![];
-    for i in start..end {
-        items.push(i)
-    }
-    items
-}
-
-// special
-fn list_get_subscript(container: &mut Vec<isize>, index: isize) -> isize {
-    container[index as usize]
-}
-
-// fn dictionary_get_subscript<K: Eq + Hash + Clone, V>(container: &mut HashMap<K, V>, key: &K) -> V {
-//     *container.get(&key).unwrap().clone()
-// }
-//
-// XXX TODO FIXME: we would really prefer to use a generic hashmap rather than
-// artificially constrain the types of Glitteral associatives or repeat the same
-// function with different types several times, but have temporarily tired of
-// beating our fists against the iron wall of "`cannot move out of borrowed
-// content`"
-fn str_int_dictionary_get_subscript(container: &mut HashMap<&str, isize>, key: &str) -> isize {
-    *container.get(key).unwrap()
-}
-
-// conjunction and disjunction
-fn and(a: bool, b: bool) -> bool {
-    a && b
-}
-fn or(a: bool, b: bool) -> bool {
-    a || b
-}
-
-// Glitteral standard library IO
-fn print_integer(n: isize) { println!(\"{}\", n); }
-fn print_integer_list(l: &mut Vec<isize>) { println!(\"{:?}\", l); }
-fn print<T: Display>(printable: T) { print!("{}", printable); }
-fn println<T: Display>(printable: T) { println!("{}", printable); }
+    with open("builtins.rs") as builtins_rs:
+        prelude = builtins_rs.read()
+    return """%s
 
 fn main() {
 %s
 }
-""" % '\n'.join(generate_expression(expression)
-                for expression in expressions)
+""" % (
+    prelude,
+    '\n'.join(generate_expression(expression)
+              for expression in expressions)
+)
